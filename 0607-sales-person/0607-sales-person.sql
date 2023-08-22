ec1a5273ce0bb81d7af6b48c6a5ebca4cdde1ffb
bhargavsairam <65115918+bhargavsairam@users.noreply.github.com>
2023-08-22 14:15:37 -0400

    Time: 3952 ms (12.59%), Space: 0B (100.00%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -5,5 +5,5 @@ select s.name from orders as o
 inner join company c on o.com_id = c.com_id 
 AND c.name = 'RED'
 right join salesPerson as s on s.sales_id = o.sales_id
-where o.sales_id is NULL
+where o.order_id is NULL
 ;
