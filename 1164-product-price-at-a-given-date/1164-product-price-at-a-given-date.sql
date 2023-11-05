386fe8bbf7d173e36b67fda54e5f68e011bc83e8
bhargavsairam <65115918+bhargavsairam@users.noreply.github.com>
2023-11-05 16:42:12 -0500

    Time: 828 ms (70.55%), Space: 0B (100.00%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,12 +1,15 @@
 # Write your MySQL query statement below
 
-SELECT p.product_id , CASE WHEN q.change_date <= '2019-08-16' THEN p.new_price ELSE 10 END as price
-FROM 
-Products p,
-(
-SELECT p.product_id, 
-IF(MIN(change_date)>'2019-08-16',MAX(change_date),(SELECT MAX(change_date) FROM Products q WHERE p.product_id = q.product_id AND change_date<='2019-08-16')) AS change_date
-FROM Products p
-GROUP BY product_id) q
-WHERE q.product_id = p.product_id
-and q.change_date = p.change_date 
+SELECT product_id, 10 as price
+from Products
+group by product_id
+having min(change_date) > "2019-08-16"
+union
+SELECT product_id, new_price as price
+from Products
+where (product_id, change_date) in (
+select product_id, max(change_date) as change_date
+from Products
+where change_date <= "2019-08-16"
+group by product_id
+)
