39c6c5f2f9b9584726a598484b486aec04f1a404
bhargavsairam <65115918+bhargavsairam@users.noreply.github.com>
2023-11-06 16:39:32 -0500

    Time: 991 ms (39.97%), Space: 0B (100.00%) - LeetHub

--- 1393-capital-gain-loss/1393-capital-gain-loss.sql
+++ 1393-capital-gain-loss/1393-capital-gain-loss.sql
@@ -1,13 +1,4 @@
 # Write your MySQL query statement below
-WITH CTE1 AS(SELECT stock_name,sum(price)  as price
+SELECT stock_name, SUM(IF(operation = 'Buy',-price,price)) AS capital_gain_loss
 FROM Stocks
-WHERE operation = 'Buy'
-GROUP BY stock_name),
-CTE2 AS (SELECT stock_name,sum(price) as price
-FROM Stocks
-WHERE operation = 'Sell'
-GROUP BY stock_name)
-
-Select CTE1.stock_name, CTE2.price - CTE1.price AS capital_gain_loss
-FROM CTE1,CTE2
-WHERE CTE1.stock_name = CTE2.stock_name
+GROUP BY stock_name
