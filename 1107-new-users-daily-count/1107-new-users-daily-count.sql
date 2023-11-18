15410c0d1acf37c9a7a4670f32ba8b10639dfda5
bhargavsairam <65115918+bhargavsairam@users.noreply.github.com>
2023-11-18 16:57:19 -0500

    Time: 698 ms (99.65%), Space: 0B (100.00%) - LeetHub

--- 1107-new-users-daily-count/1107-new-users-daily-count.sql
+++ 1107-new-users-daily-count/1107-new-users-daily-count.sql
@@ -2,10 +2,10 @@
 
 SELECT login_date, count(DISTINCT user_id) AS user_count FROM
 (
-    SELECT distinct user_id, MIN(activity_date) over (partition by user_id, activity) as login_date
+    SELECT distinct user_id, MIN(activity_date)  as login_date
 FROM Traffic
 WHERE activity = 'login'
-#GROUP BY user_id
+GROUP BY user_id
 ) q
 WHERE DATEDIFF('2019-06-30',login_date) <= 90
 GROUP BY login_date
