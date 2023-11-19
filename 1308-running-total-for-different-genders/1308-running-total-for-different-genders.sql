f8e551477d88f956dc92cf2fccc04fc12f470fea
bhargavsairam <65115918+bhargavsairam@users.noreply.github.com>
2023-11-19 16:57:24 -0500

    Time: 1333 ms (20.00%), Space: 0B (100.00%) - LeetHub

--- 1308-running-total-for-different-genders/1308-running-total-for-different-genders.sql
+++ 1308-running-total-for-different-genders/1308-running-total-for-different-genders.sql
@@ -1,5 +1,3 @@
 # Write your MySQL query statement below
 SELECT gender, day, sum(score_points) over(partition by gender order by day asc) as total
 FROM Scores
-Group By gender,day
-ORDER BY gender, day asc
