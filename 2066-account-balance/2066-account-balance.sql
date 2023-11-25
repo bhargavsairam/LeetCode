57f009e92182e81d3f88c614357316f99d5d3e04
bhargavsairam <65115918+bhargavsairam@users.noreply.github.com>
2023-11-25 18:36:31 -0500

    Time: 1132 ms (11.36%), Space: 0B (100.00%) - LeetHub

---
+++ 2066-account-balance/2066-account-balance.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT account_id, day, 
+SUM(CASE WHEN type = 'Deposit' THEN amount 
+ELSE -amount END) OVER(partition by account_id order by day) AS balance
+FROM Transactions
+GROUP BY account_id, day
+ORDER BY account_id, day
