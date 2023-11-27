0e88ff6f8db8015a16c8063f9a5ab017791b3b28
bhargavsairam <65115918+bhargavsairam@users.noreply.github.com>
2023-11-27 12:36:14 -0500

    Time: 957 ms (36.52%), Space: 0B (100.00%) - LeetHub

--- 2308-arrange-table-by-gender/2308-arrange-table-by-gender.sql
+++ 2308-arrange-table-by-gender/2308-arrange-table-by-gender.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 SELECT user_id, gender
 FROM Genders
-ORDER BY dense_rank() over(partition by gender order by user_id asc), length(gender) desc
+ORDER BY rank() over(partition by gender order by user_id asc), length(gender) desc
