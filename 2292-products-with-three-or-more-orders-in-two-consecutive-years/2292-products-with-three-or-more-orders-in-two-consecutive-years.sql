178e6501443b546df5aa3910237163d21e7ee194
bhargavsairam <65115918+bhargavsairam@users.noreply.github.com>
2023-11-27 12:10:04 -0500

    Time: 951 ms (51.18%), Space: 0B (100.00%) - LeetHub

--- 2292-products-with-three-or-more-orders-in-two-consecutive-years/2292-products-with-three-or-more-orders-in-two-consecutive-years.sql
+++ 2292-products-with-three-or-more-orders-in-two-consecutive-years/2292-products-with-three-or-more-orders-in-two-consecutive-years.sql
@@ -5,7 +5,6 @@ FROM Orders
 GROUP BY product_id, year(purchase_date)
 having sum1 >=3)
 
-SELECT distinct product_id
-FROM (SELECT product_id, year1,lead(year1) over(partition by product_id order by year1) as lead_year
-FROM CTE) q
-WHERE lead_year - year1 = 1
+SELECT Distinct c1.product_id 
+FROM CTE c1 JOIN CTE c2
+ON c1.product_id = c2.product_id and c1.year1 = c2.year1+1
