20a6a0996ec61644c02924ad8501c230b3e7cfbe
bhargavsairam <65115918+bhargavsairam@users.noreply.github.com>
2023-11-28 09:58:00 -0500

    Time: 50 ms (91.08%), Space: 16.1 MB (78.84%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,7 +1,4 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         list1 = list(a for a in str(x))
-        if list1 == list1[::-1]:
-            return True
-        else:
-            False
+        return list1 == list1[::-1]
