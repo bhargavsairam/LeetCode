a2a01212bc2669f5b65af4d114099dc06bddb0de
bhargavsairam <65115918+bhargavsairam@users.noreply.github.com>
2023-11-29 09:09:24 -0500

    Time: 1956 ms (85.28%), Space: 0B (100.00%) - LeetHub

--- 2372-calculate-the-influence-of-each-salesperson/2372-calculate-the-influence-of-each-salesperson.sql
+++ 2372-calculate-the-influence-of-each-salesperson/2372-calculate-the-influence-of-each-salesperson.sql
@@ -3,4 +3,4 @@ SELECT p.salesperson_id,p.name ,IFNULL(sum(s.price),0) as total
 FROM Salesperson p 
 LEFT JOIN Customer c  ON p.salesperson_id = c.salesperson_id
 LEFT JOIN Sales s ON c.customer_id = s.customer_id
-GROUP BY p.salesperson_id, p.name
+GROUP BY p.salesperson_id
