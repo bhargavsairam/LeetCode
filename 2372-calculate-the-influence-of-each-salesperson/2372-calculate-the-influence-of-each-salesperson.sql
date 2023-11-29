b6af042f6b24c424f2b3bc133fd7518f7f701a3e
bhargavsairam <65115918+bhargavsairam@users.noreply.github.com>
2023-11-29 09:09:10 -0500

    Time: 2288 ms (36.36%), Space: 0B (100.00%) - LeetHub

---
+++ 2372-calculate-the-influence-of-each-salesperson/2372-calculate-the-influence-of-each-salesperson.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT p.salesperson_id,p.name ,IFNULL(sum(s.price),0) as total
+FROM Salesperson p 
+LEFT JOIN Customer c  ON p.salesperson_id = c.salesperson_id
+LEFT JOIN Sales s ON c.customer_id = s.customer_id
+GROUP BY p.salesperson_id, p.name
