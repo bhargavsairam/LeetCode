c2449b5c8fa6db1a48011c741498c6a7fa8d8a5c
bhargavsairam <65115918+bhargavsairam@users.noreply.github.com>
2024-06-30 13:29:30 -0400

    Time: 111 ms (52.42%), Space: 19.1 MB (52.58%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hashtable = defaultdict(int)
+        for i in nums:
+            hashtable[i] +=1
+        for i in hashtable:
+            if hashtable[i] == 1:
+                return i
+        
+            
+            
+            
